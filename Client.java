60eb565a9fb84ae393122c98e048e1502db2f9c4
Cole Garrison <cole.garrison@gwu.edu>
2024-11-06 16:24:25 -0500

    Add files via upload

--- Client.java
+++ Client.java
@@ -1,3 +1,27 @@
+import java.net.Socket;
+
 public class Client {
-    
+
+    public Client(String idk, int smthn) {
+
+
+    }
+
+    public Socket getSocket() {
+        return null;
+    }
+
+    public void handshake() {
+
+    }
+
+    public void disconnect() {
+
+    }
+
+    public String request(String idk) {
+
+        return null;
+    }
 }
+
--- Server.java
+++ Server.java
@@ -1,3 +1,22 @@
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+
 public class Server {
+
+    public Server(int code) {
+
+    }
+
+    public void serve(int idk) {
+
+    }
+
+    public void disconnect() {
+
+    }
+
+    public ArrayList<LocalDateTime> getConnectedTimes() {
+        return null;
+    }
     
 }
